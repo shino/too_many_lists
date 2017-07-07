b5f55aa3ec87b32d84b0aa572a10fcecd6e057e1
Shunichi Shinohara <shino.shun@gmail.com>
2017-07-07 15:39:15 +0900

    3.1 Option

--- src/second.rs
+++ src/second.rs
@@ -1,13 +1,8 @@
-use std::mem;
-
 pub struct List {
     head: Link,
 }
 
-enum Link {
-    Empty,
-    More(Box<Node>),
-}
+type Link = Option<Box<Node>>;
 
 struct Node {
     elem: i32,
@@ -18,39 +13,36 @@ struct Node {
 impl List {
 
     pub fn new() -> Self {
-        List { head: Link::Empty }
+        List { head: None }
     }
 
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, Link::Empty),
+            // https://doc.rust-lang.org/std/option/enum.Option.html#method.take
+            next: self.head.take(),
         });
 
-        self.head = Link::More(new_node);
+        self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, Link::Empty) {
-            Link::Empty => None,
-            Link::More(boxed_node) => {
-                let node = *boxed_node;
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+        // https://doc.rust-lang.org/std/option/enum.Option.html#method.map
+        self.head.take().map(|boxed_node| {
+            // move node into this frame, which should be deleted when
+            // this function finishes.
+            let node = *boxed_node;
+            self.head = node.next;
+            node.elem
+        })
     }
 }
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
-        // `while let` == "do this thing until this pattern does't match"
-        while let Link::More(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
-            // boxed_node goes out of scope and gets dropped here;
-            // but its Node's `next` field has been set to Link::Empty
-            // so no unbounded recursion occurs.
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
         }
     }
 }
