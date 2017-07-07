6147f1d57918c48b9ab3d17dac05ab8053967d83
Shunichi Shinohara <shino.shun@gmail.com>
2017-07-07 16:33:23 +0900

    3.4 IntoIter

--- src/second.rs
+++ src/second.rs
@@ -9,7 +9,6 @@ struct Node<T> {
     next: Link<T>,
 }
 
-
 impl<T> List<T> {
 
     pub fn new() -> Self {
@@ -59,6 +58,21 @@ impl<T> Drop for List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -101,6 +115,18 @@ mod test {
 
         assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
 
