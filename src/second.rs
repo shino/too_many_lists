34dff50a0713f3040e3a7d3856b4c46d04aa1e2b
Shunichi Shinohara <shino.shun@gmail.com>
2017-07-07 17:26:30 +0900

    3.5 Iter

--- src/second.rs
+++ src/second.rs
@@ -73,6 +73,26 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -127,6 +147,28 @@ mod test {
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
+
+        //// error[E0382]: use of moved value: `list`
+        // let mut iter2 = list.iter();
+        // assert_eq!(iter2.next(), Some(&3));
+
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+
+        // `iter()` can be called here, unlike `into_iter()`
+        let mut iter2 = list.iter();
+        assert_eq!(iter2.next(), Some(&3));
+
     }
 }
 
