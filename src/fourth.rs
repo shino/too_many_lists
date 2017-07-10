3dfeb6890f16d15dea1ed97d3777ee9596f52e1f
Shunichi Shinohara <shino.shun@gmail.com>
2017-07-10 16:13:05 +0900

    5.5 Symmetric Junk

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,5 +1,5 @@
 use std::rc::Rc;
-use std::cell::{Ref, RefCell};
+use std::cell::{RefCell, Ref, RefMut};
 
 pub struct List<T> {
     head: Link<T>,
@@ -49,6 +49,21 @@ impl<T> List<T> {
         }
     }
 
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+    }
+
     pub fn pop_front(&mut self) -> Option<T> {
         // need to take the old head, ensureing it's -1
         self.head.take().map(|old_head| {              // -1 old
@@ -58,7 +73,7 @@ impl<T> List<T> {
                     self.head = Some(new_head);        // +1 new
                 }
                 None => {
-                    self.tail = None                   // -1 old
+                    self.tail = None;                  // -1 old
                 }
             };
             // Grab old_head by value, because it should be dropped.
@@ -75,6 +90,21 @@ impl<T> List<T> {
         })
     }
 
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head = None;
+                }
+            }
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
+
     pub fn peek_front(&self) -> Option<Ref<T>> {
         // impl<'b, T> Ref<'b, T> where T: ?Sized,
         //   `fn map<U, F>(orig: Ref<'b, T>, f: F) -> Ref<'b, U>`
@@ -87,6 +117,23 @@ impl<T> List<T> {
         })
     }
 
+    pub fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
+        self.head.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
+
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
+
+    pub fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
+        self.tail.as_ref().map(|node| {
+            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -112,7 +159,7 @@ mod test {
         list.push_front(3);
 
         // Peek
-        assert_eq!(*list.peek_front().unwrap(), 3);
+        assert_eq!(&*list.peek_front().unwrap(), &3);
 
         // Check normal removal
         assert_eq!(list.pop_front(), Some(3));
@@ -129,5 +176,50 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+
+        // -- back
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_back(), None);
+
+        // Populate list
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        // Peek
+        assert_eq!(&*list.peek_back().unwrap(), &3);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_back(4);
+        list.push_back(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+        assert!(list.peek_back().is_none());
+        assert!(list.peek_front_mut().is_none());
+        assert!(list.peek_back_mut().is_none());
+
+        list.push_front(1); list.push_front(2); list.push_front(3);
+
+        assert_eq!(&*list.peek_front().unwrap(), &3);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+        assert_eq!(&*list.peek_back_mut().unwrap(), &mut 1);
     }
 }
