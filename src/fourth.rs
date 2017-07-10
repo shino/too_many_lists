2b2b5170c105e5ca2ac58f27bb6dda5f62d891be
Shunichi Shinohara <shino.shun@gmail.com>
2017-07-10 12:41:37 +0900

    5.2 Building Up

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::cell::Cell;
 
 pub struct List<T> {
     head: Link<T>,
@@ -12,4 +13,41 @@ struct Node<T> {
     elem: T,
     next: Link<T>,
     prev: Link<T>,
+}
+
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem: elem,
+            next: None,
+            prev: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links, everything else should be +0
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                self.head = Some(new_head.clone());
+                self.tail = Some(new_head);
+            }
+        }
+    }
+
 }
